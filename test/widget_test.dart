286a570799f667a9c39dab10fbb3f5951b70de66
조정규 <regularcho1009@metabuild.co.kr>
2025-06-20 16:40:35 +0900

    reset: 프로젝트 초기 상태로 롤백

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:custom_desktop/core/app/app.dart';
+import 'package:custom_desktop/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
